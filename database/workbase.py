a3b56966dc22521cb6cf1c19f131d5b3943f9347
AsQ <danila.udodov.workmail@gmail.com>
2023-06-02 23:14:08 +0300

    Добавлены новые функции для работы с базой данных

--- database/workbase.py
+++ database/workbase.py
@@ -33,3 +33,31 @@ class client_control():
                     VALUES (?, ?, ?, ?, ?, ?)", (user_id, first_name, last_name, full_name, user_name, message_id,))
         except Exception as e:
             error_(__file__, e)
+    
+    def update_old_info(self, user_id, first_name, last_name, full_name, user_name):
+        try:
+            info_(__file__, 'Запрос в базу данных update_old_info')
+            with self.connection:
+                return self.cursor.execute(f"UPDATE {self.main_table_name} SET first_name = ?, last_name = ?, full_name = ?, user_name = ? WHERE user_id = ?",
+                    (first_name, last_name, full_name, user_name, user_id,))
+        except Exception as e:
+            error_(__file__, e)
+    
+    def select_message_id(self, user_id):
+        try:
+            info_(__file__, 'Запрос в базу данных select_message_id')
+            with self.connection:
+                select = self.cursor.execute(f"SELECT message_id FROM {self.main_table_name} WHERE user_id = ?", (user_id,)).fetchmany(1)
+                return select[0][0]
+        except Exception as e:
+            error_(__file__, e)
+    
+    def update_message_id(self, user_id, message_id):
+        try:
+            info_(__file__, 'Запрос в базу данных update_message_id')
+            with self.connection:
+                return self.cursor.execute(f"UPDATE {self.main_table_name} SET message_id = ? WHERE user_id = ?",
+                    (message_id, user_id,))
+        except Exception as e:
+            error_(__file__, e)
+    
